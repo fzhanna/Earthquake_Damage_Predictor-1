7e5e0acdfb2838c7392331e4161d680cccd43ec1
muhammad ali <muhammadaliog3@live.com>
2020-12-22 13:16:12 -0500

    Undestanding the types and shapes of the data

--- Helpers/Load_Raw_Data.R
+++ Helpers/Load_Raw_Data.R
@@ -1,3 +1,9 @@
-loadTrainVal <- function() {
-  return(read.csv('Raw_Data/train_values.csv'))
+trainVal <- function() {
+  return(utils::read.csv('Raw_Data/train_values.csv'))
+}
+testVal <- function() {
+  return(utils::read.csv('Raw_Data/test_values.csv'))
+}
+trainLab <- function() {
+  return(utils::read.csv('Raw_Data/train_labels.csv'))
 }
